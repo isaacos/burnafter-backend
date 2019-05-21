982c8e1fd03546996905ed99c0029d7c9c5d4e69
isaac <oseasisaac91@gmail.com>
2019-05-21 14:28:41 -0400

    set up action cable for newusers.

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,2 +1,13 @@
 class MessagesController < ApplicationController
+
+  def create
+    @message = Message.create(message_params)
+    render json: @message
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:chat_id, :user_id, :text)
+  end
 end
