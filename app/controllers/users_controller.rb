14c60e7177d52bb84636d3f2e989bc5380cdcd1e
isaac <oseasisaac91@gmail.com>
2019-06-12 23:11:08 -0400

    user delete and it deletes all chats they are associated with and all messages associated with that chat

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,6 +11,24 @@ class UsersController < ApplicationController
     render json: @users
   end
 
+  def destroy
+    @user = User.find(params[:id])
+
+    chats = @user.chats
+
+    chats.each do |chat|
+      message = Message.create(chat_id: chat.id, user_id: @user.id, text: 'chat closed')
+      MessageChannel.broadcast_to(chat, message)
+
+      chat.messages.each do |message|
+        message.destroy
+      end
+      chat.destroy
+    end
+
+    @user.destroy
+  end
+
   private
 
   def user_params
