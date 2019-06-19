41b867a3e2e0cc2bc4e3710047c4aa1385a24623
isaac <oseasisaac91@gmail.com>
2019-06-19 14:21:21 -0400

    User is deleted with associated data, including all orphaned messages (those without at Chat, but can still have a user)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -13,19 +13,13 @@ class UsersController < ApplicationController
 
   def destroy
     @user = User.find(params[:id])
-
     chats = @user.chats
-
     chats.each do |chat|
       message = Message.create(chat_id: chat.id, user_id: @user.id, text: 'chat closed')
       MessageChannel.broadcast_to(chat, message)
-
-      chat.messages.each do |message|
-        message.destroy
-      end
-      chat.destroy
     end
-
+    Chat.where('`chats`.`first_user` IN (?) OR `chats`.`second_user` IN (?)', @user.id, @user.id).delete_all
+    Message.left_outer_joins(:chat).where(chats: {id: nil}).delete_all
     @user.destroy
   end
 
