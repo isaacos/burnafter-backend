9d5331374717996ad5995d359e1c095285ebbe7c
isaac <oseasisaac91@gmail.com>
2019-08-02 09:32:06 -0400

    removed Channel in the create user method. Added a Thread inorder to call the delete user after a certain amount of time, which I put in the User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,16 @@
 class User < ApplicationRecord
   has_many :messages
   has_many :chats, through: :messages
+  @time_until_deletion = 0
+
+  def set_time_until_deletion
+    @time_until_deletion = 30
+    while @time_until_deletion > 0
+      @time_until_deletion -= 1
+      sleep 1
+      if @time_until_deletion == 0
+        self.destroy
+      end
+    end
+  end
 end
